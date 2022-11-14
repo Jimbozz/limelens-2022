c22651c7a6df049a78ce8efaaa152c698d01eadd
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-11-14 15:56:26 +0100

    added GSAP animations

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,6 @@
+//Mobile nav
+//-------------------------------------------
+
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".navbar__menu");
 const logo = document.querySelector("#navbar__logo");
@@ -12,3 +15,51 @@ const mobileMenu = () => {
 };
 
 menu.addEventListener("click", mobileMenu);
+
+// GSAP animations
+
+let mm = gsap.matchMedia();
+
+mm.add("(min-width: 800px)", () => {
+  gsap.utils.toArray(".btn").forEach((btn) => {
+    let hover = gsap.to(btn, {
+      backgroundColor: "#353535",
+      duration: 0.3,
+      paused: true,
+      ease: "power1.inOut",
+    });
+    btn.addEventListener("mouseenter", () => hover.play());
+    btn.addEventListener("mouseleave", () => hover.reverse());
+  });
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.from(".hero-heading", {
+    duration: 0.9,
+    opacity: 0,
+    x: -200,
+  });
+
+  const horizontalSection = document.querySelector(".horizontal");
+
+  gsap.to(".horizontal", {
+    x: () => horizontalSection.scrollWidth * -1,
+    xPercent: 100,
+    scrollTrigger: {
+      trigger: ".horizontal",
+      start: "center center",
+      end: "+=1000",
+      pin: ".animation-container",
+      scrub: true,
+      invalidateOnRefresh: true,
+      scrub: 1.5,
+    },
+  });
+
+  gsap.from(".about-sticky", {
+    scrollTrigger: ".about-sticky",
+    duration: 0.9,
+    opacity: 0,
+    x: -200,
+  });
+});
